4e914f08cf93337d7c0cec46237daea1494a0b97
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2025-10-04 00:04:40 +0200

    initial

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    rules: {
+      // already handled by typescript (TS2300, TS2323)
+      'import/export': 'off',
+      // already handled by typescript (TS1192)
+      'import/default': 'off',
+      // already handled by typescript (TS2614)
+      'import/named': 'off',
+      // already handled by typescript (TS2307)
+      'import/no-unresolved': 'off',
+      'import/no-extraneous-dependencies': 'error',
+      'import/extensions': ['error', 'never'],
+      'import/order': [
+        'error',
+        {
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: import.meta.resolve('./tsconfig.json'),
+        },
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
